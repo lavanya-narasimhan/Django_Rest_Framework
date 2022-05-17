79af86998a824d74a915edd7fb3028f9bbaec51b
Lavanya Narasimhan <lnarasim@usc.edu>
2022-05-16 18:13:56 -0700

    feat (Django Rest Framework): Code Clean up
    Removed unused import lines and followed PEP8 guidelines to clean up code

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -5,14 +5,16 @@ from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    highlight= serializers.HyperlinkedIdentityField(view_name= 'snippet-highlight', format= 'html')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner', 'url', 'highlight']
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    snippets= serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
-    class Meta:
-        model= User
-        fields= ['id', 'username', 'snippets', 'url']
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets', 'url']
--- snippets/views.py
+++ snippets/views.py
@@ -1,12 +1,9 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework import renderers
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -18,23 +15,22 @@ class SnippetViewSet(viewsets.ModelViewSet):
     update and destroy actions/
     Additionally we also provide an extra highlight action
     '''
-    queryset= Snippet.objects.all()
-    serializer_class= SnippetSerializer
-    permission_classes= [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
-    @action(detail= True, renderer_classes= [renderers.StaticHTMLRenderer])
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
-        snippet= self.get_object()
+        snippet = self.get_object()
         return Response(snippet.highlighted)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     '''
     This viewset automatically provides list and retrieve actions
     '''
-    queryset= User.objects.all()
-    serializer_class= UserSerializer
-
-
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
